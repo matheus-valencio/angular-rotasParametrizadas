f940dbef5bbca38cc7bcf4d5805809ba19373a21
matheus-valencio <matheus.valencio@live.com>
2022-03-11 20:41:27 -0300

    3 files modified, 1 added

--- src/app/list-details/list-details.component.ts
+++ src/app/list-details/list-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { listData } from '../list';
 
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.css']
+  styleUrls: ['./list-details.component.css'],
 })
 export class ListDetailsComponent implements OnInit {
-
-  constructor() { }
+  item;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.item = listData[params.get('index')];
+    });
   }
-
 }
---
+++ src/app/list.ts
@@ -0,0 +1,5 @@
+export const listData = [
+  { id: 1, text: 'Texto um' },
+  { id: 2, text: 'Texto dois' },
+  { id: 3, text: 'Texto três' },
+];
